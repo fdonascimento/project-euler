f047b5898adab9ab36ff41693275049523cea80b
Fagner Fonseca <fagneolli@gmail.com>
2020-06-28 14:20:45 -0300

    [Euler14] Add solution to calculate based on Stack and LinkedHashSet.

--- src/main/java/fagner/project_euler/LongestCollatzSequence.java
+++ src/main/java/fagner/project_euler/LongestCollatzSequence.java
@@ -2,8 +2,7 @@ package fagner.project_euler;
 
 import fagner.project_euler.util.CollatzSequence;
 
-import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.*;
 
 public class LongestCollatzSequence {
 
@@ -22,10 +21,83 @@ public class LongestCollatzSequence {
         return largestSequence.getStartNumber();
     }
 
+    public int calculate2(int lastNumber) {
+        int largestSequence = 1;
+        int numberLargestSequence = 1;
+
+        Stack<Integer> items = new Stack<>();
+        for (int i = 1; i <= lastNumber; i++) {
+            items.push(i);
+        }
+
+        int iterations = 0;
+        while(!items.empty()) {
+            iterations++;
+            int startNumber = items.pop();
+
+            int nextTerm = startNumber;
+            int countSequence = 1;
+            while(nextTerm != 1) {
+                if (nextTerm % 2 == 0) {
+                    nextTerm =  nextTerm / 2;
+                } else {
+                    nextTerm = (nextTerm * 3) + 1;
+                }
+                countSequence++;
+                items.removeElement(nextTerm);
+            }
+
+            if (countSequence > largestSequence) {
+                numberLargestSequence = startNumber;
+                largestSequence = countSequence;
+            }
+        }
+
+        System.out.println("Iterations = "+iterations);
+        return numberLargestSequence;
+    }
+
+    public int calculate3(int lastNumber) {
+        int largestSequence = 1;
+        int numberLargestSequence = 1;
+
+        LinkedHashSet<Integer> items = new LinkedHashSet<>();
+        for (int i = lastNumber; i >= 1; i--) {
+            items.add(i);
+        }
+
+        int iterations = 0;
+        while(!items.isEmpty()) {
+            iterations++;
+            int startNumber = items.stream().findFirst().get();
+            items.remove(startNumber);
+
+            int nextTerm = startNumber;
+            int countSequence = 1;
+            while(nextTerm != 1) {
+                if (nextTerm % 2 == 0) {
+                    nextTerm =  nextTerm / 2;
+                } else {
+                    nextTerm = (nextTerm * 3) + 1;
+                }
+                countSequence++;
+                items.remove(nextTerm);
+            }
+
+            if (countSequence > largestSequence) {
+                numberLargestSequence = startNumber;
+                largestSequence = countSequence;
+            }
+        }
+
+        System.out.println("Iterations = "+iterations);
+        return numberLargestSequence;
+    }
+
     public static void main(String[] args) {
         LongestCollatzSequence calculator = new LongestCollatzSequence();
         long startTime = Calendar.getInstance().getTimeInMillis();
-        int result = calculator.calculate(200_000);
+        int result = calculator.calculate3(500_000);
         long endTime = Calendar.getInstance().getTimeInMillis();
 		System.out.println("Result = "+result +" | time in ms = "+(endTime-startTime));
     }
