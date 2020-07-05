ab5aa26a65b31a71995da8499f7addc20d13ffe2
Fagner Fonseca <fagneolli@gmail.com>
2020-07-05 14:48:08 -0300

    [Euler15] Add a faster method, however the logic is incorrect.

--- src/main/java/fagner/project_euler/LatticePaths.java
+++ src/main/java/fagner/project_euler/LatticePaths.java
@@ -1,26 +1,58 @@
 package fagner.project_euler;
 
-import org.apache.commons.math3.linear.RealMatrix;
-
 import java.util.Calendar;
+import java.util.HashMap;
 
+/**
+ * @since 02/07/2020
+ */
 public class LatticePaths {
+
+    private HashMap<String, Long> routesMap = new HashMap<>();
 
     public long calculate(int gridSize) {
         return calculateRoutes(0, 0, gridSize);
+//        return calculateRoutes2(0, 0, gridSize);
     }
 
-    public long calculateRoutes(int row, int column, int gridSize) {
+    public long calculateRoutes(int row, int column, int lastIndex) {
+        if (row == lastIndex && column == lastIndex) {
+            return 1;
+        } else {
+            long routes = 0;
+            if (row < lastIndex) {
+                routes += getNumberOfRoutes(row+1, column, lastIndex);
+            }
+            if (column < lastIndex) {
+                routes += getNumberOfRoutes(row, column+1, lastIndex);
+            }
+            return routes;
+        }
+    }
+
+    public long calculateRoutes2(int row, int column, int gridSize) {
         if (row == gridSize && column == gridSize) {
             return 1;
         } else {
             long routes = 0;
             if (row < gridSize) {
-                routes += calculateRoutes(row+1, column, gridSize);
+                routes += calculateRoutes2(row+1, column, gridSize);
             }
             if (column < gridSize) {
-                routes += calculateRoutes(row, column+1, gridSize);
+                routes += calculateRoutes2(row, column+1, gridSize);
             }
+            return routes;
+        }
+    }
+
+    private long getNumberOfRoutes(int row, int column, int lastIndex) {
+        String key = String.valueOf(row).concat(String.valueOf(column));
+        Long subRoutes = routesMap.get(key);
+        if (subRoutes != null) {
+            return subRoutes;
+        } else {
+            long routes = calculateRoutes(row, column, lastIndex);
+            routesMap.put(key, routes);
             return routes;
         }
     }
@@ -28,7 +60,7 @@ public class LatticePaths {
     public static void main(String[] args) {
         LatticePaths calculator = new LatticePaths();
         long startTime = Calendar.getInstance().getTimeInMillis();
-        long result = calculator.calculate(16);
+        long result = calculator.calculate(10);
         long endTime = Calendar.getInstance().getTimeInMillis();
         System.out.println("Result = "+result +" | time in ms = "+(endTime-startTime));
     }
--- src/test/java/fagner/project_euler/LatticePathsTest.java
+++ src/test/java/fagner/project_euler/LatticePathsTest.java
@@ -7,8 +7,50 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LatticePathsTest {
 
     @Test
-    public void calculateGrid20Then6() {
+    public void calculateGrid2Then6() {
         LatticePaths calculator = new LatticePaths();
         assertThat(calculator.calculate(2)).isEqualTo(6);
+    }
+
+    @Test
+    public void calculateGrid3Then20() {
+        LatticePaths calculator = new LatticePaths();
+        assertThat(calculator.calculate(3)).isEqualTo(20);
+    }
+
+    @Test
+    public void calculateGrid4Then70() {
+        LatticePaths calculator = new LatticePaths();
+        assertThat(calculator.calculate(4)).isEqualTo(70);
+    }
+
+    @Test
+    public void calculateGrid15Then155117520() {
+        LatticePaths calculator = new LatticePaths();
+        assertThat(calculator.calculate(15)).isEqualTo(155117520);
+    }
+
+    @Test
+    public void calculateGrid14Then40116600() {
+        LatticePaths calculator = new LatticePaths();
+        assertThat(calculator.calculate(14)).isEqualTo(40116600);
+    }
+
+    @Test
+    public void calculateGrid13Then10400600() {
+        LatticePaths calculator = new LatticePaths();
+        assertThat(calculator.calculate(13)).isEqualTo(10400600);
+    }
+
+    @Test
+    public void calculateGrid12Then2704156() {
+        LatticePaths calculator = new LatticePaths();
+        assertThat(calculator.calculate(12)).isEqualTo(2704156);
+    }
+
+    @Test
+    public void calculateGrid11Then705432() {
+        LatticePaths calculator = new LatticePaths();
+        assertThat(calculator.calculate(11)).isEqualTo(705432);
     }
 }
