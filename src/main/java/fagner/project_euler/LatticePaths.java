6ceae8c34a720252bd2149cbae862fb3ecd4fbe3
Fagner Fonseca <fagneolli@gmail.com>
2020-07-10 00:02:41 -0300

    [Euler15] For some reason, creating the map key as [rowcolumn], eg. [12] doesn't work. The key was being generated incorrectly. Replacing the key for [row:colum], eg. [1:2], solves the problem.

--- src/main/java/fagner/project_euler/LatticePaths.java
+++ src/main/java/fagner/project_euler/LatticePaths.java
@@ -12,7 +12,6 @@ public class LatticePaths {
 
     public long calculate(int gridSize) {
         return calculateRoutes(0, 0, gridSize);
-//        return calculateRoutes2(0, 0, gridSize);
     }
 
     public long calculateRoutes(int row, int column, int lastIndex) {
@@ -30,23 +29,8 @@ public class LatticePaths {
         }
     }
 
-    public long calculateRoutes2(int row, int column, int gridSize) {
-        if (row == gridSize && column == gridSize) {
-            return 1;
-        } else {
-            long routes = 0;
-            if (row < gridSize) {
-                routes += calculateRoutes2(row+1, column, gridSize);
-            }
-            if (column < gridSize) {
-                routes += calculateRoutes2(row, column+1, gridSize);
-            }
-            return routes;
-        }
-    }
-
     private long getNumberOfRoutes(int row, int column, int lastIndex) {
-        String key = String.valueOf(row).concat(String.valueOf(column));
+        String key = String.format("%d:%d", row, column);
         Long subRoutes = routesMap.get(key);
         if (subRoutes != null) {
             return subRoutes;
@@ -60,34 +44,8 @@ public class LatticePaths {
     public static void main(String[] args) {
         LatticePaths calculator = new LatticePaths();
         long startTime = Calendar.getInstance().getTimeInMillis();
-        long result = calculator.calculate(10);
+        long result = calculator.calculate(20);
         long endTime = Calendar.getInstance().getTimeInMillis();
         System.out.println("Result = "+result +" | time in ms = "+(endTime-startTime));
-    }
-}
-
-class Node {
-    private static int routes;
-    private int row;
-    private int column;
-    private Node right;
-    private Node down;
-
-    public Node(int row, int column) {
-        this.row = row;
-        this.column = column;
-    }
-
-    public void walkUntil(int lastIndex) {
-        if (row == lastIndex && column == lastIndex) {
-            routes++;
-        } else {
-            if (row < lastIndex) {
-                new Node(row + 1, column).walkUntil(lastIndex);
-            }
-            if (column < lastIndex) {
-                new Node(row, column + 1).walkUntil(lastIndex);
-            }
-        }
     }
 }
