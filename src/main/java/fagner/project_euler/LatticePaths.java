15edf1b043f63b07c9a794960abaac91d20af199
Fagner Fonseca <fagneolli@gmail.com>
2020-07-12 14:17:11 -0300

    [Euler15] Add time complexity information

--- src/main/java/fagner/project_euler/LatticePaths.java
+++ src/main/java/fagner/project_euler/LatticePaths.java
@@ -4,14 +4,19 @@ import java.util.Calendar;
 import java.util.HashMap;
 
 /**
+ * Time Complexity = O(log(n))
+ * The number of routes grow exponentially, but the number of iterations grow linearly.
  * @since 02/07/2020
  */
 public class LatticePaths {
 
     private HashMap<String, Long> routesMap = new HashMap<>();
+    private int iterations = 0;
 
     public long calculate(int gridSize) {
-        return calculateRoutes(0, 0, gridSize);
+        long result = calculateRoutes(0, 0, gridSize);
+        System.out.println("Number of iterations: "+ iterations);
+        return result;
     }
 
     public long calculateRoutes(int row, int column, int lastIndex) {
@@ -35,6 +40,7 @@ public class LatticePaths {
         if (subRoutes != null) {
             return subRoutes;
         } else {
+            iterations++;
             long routes = calculateRoutes(row, column, lastIndex);
             routesMap.put(key, routes);
             return routes;
@@ -44,7 +50,7 @@ public class LatticePaths {
     public static void main(String[] args) {
         LatticePaths calculator = new LatticePaths();
         long startTime = Calendar.getInstance().getTimeInMillis();
-        long result = calculator.calculate(20);
+        long result = calculator.calculate(100);
         long endTime = Calendar.getInstance().getTimeInMillis();
         System.out.println("Result = "+result +" | time in ms = "+(endTime-startTime));
     }
